761b21a77eec1cff3c3321f3c321f5c525e0e65c
MENNJ <2804700263@qq.com>
2024-08-08 11:15:54 +0800

    畢業設計

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <NavBar/>
+  <router-view />
+</template>
+
+<script lang="ts">
+import NavBar from "./views/Nav_bar.vue";
+
+export default {
+  components: {
+    NavBar,
+  },
+  data() {
+    return {
+      currentTheme: "light",
+      isDark: false,
+    };
+  },
+};
+</script>
